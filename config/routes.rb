9ba9a5d2a3633ac5c07463423eee53ad1b00d77c
Gabriel Mathias <gablemathias@icloud.com>
2024-02-24 19:12:55 +1100

    adding CRUD options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"     # main articles page
-  get "/articles/:id", to: "articles#show"  # show each article
+  resources :articles # map all the 7 routes for the given resource
 end
